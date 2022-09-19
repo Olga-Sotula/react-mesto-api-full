6e8074dde990aedae2fcff202a546117eb3e8f29
Olga Sotula <olga.sotula@itctg.ru>
2022-09-19 15:20:08 +0300

    add crash test

--- backend/app.js
+++ backend/app.js
@@ -27,6 +27,13 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(bodyParser.json());
 
 app.use(requestLogger); // подключаем логгер запросов
+
+app.get('/crash-test', () => { // crash-test
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use(authRouter);
 app.use(auth);
 app.use(userRouter);
