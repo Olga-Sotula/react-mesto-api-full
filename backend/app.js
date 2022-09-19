6f20370e16aaa8cb4891d75b2937d9288742227b
Olga Sotula <olga.sotula@itctg.ru>
2022-09-19 15:10:15 +0300

    add requestLogger, errorLogger

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,7 @@ const { cardRouter } = require('./routes/cards');
 const { authRouter } = require('./routes/auth');
 const { errorHandler } = require('./errors/errorHandler');
 const { ErrorNotFound } = require('./errors/ErrorNotFound');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
@@ -25,6 +26,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use(bodyParser.json());
 
+app.use(requestLogger); // подключаем логгер запросов
 app.use(authRouter);
 app.use(auth);
 app.use(userRouter);
@@ -35,6 +37,7 @@ app.all('*', (req, res, next) => {
   next(new ErrorNotFound('Указанный маршрут не существует'));
 });
 
+app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
